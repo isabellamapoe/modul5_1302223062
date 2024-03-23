8ff49055fcd687c45f5c685f9f15766727927630
rifkyfrds <rifky.frds@gmail.com>
2024-03-23 09:04:24 +0700

    menambahkan class SimpleDataBase

--- modul5_1302223062/Program.cs
+++ modul5_1302223062/Program.cs
@@ -2,15 +2,41 @@
 using System.Net.NetworkInformation;
 using System.Numerics;
 
-class penjumlahan 
+class SimpleDataBase <T>
 {
-    public static T JumlahTigaAngka<T>(T satu, T Dua, T tiga) where T : IAdditionOperators<T,T,T>
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
     {
-        return satu + Dua + tiga;
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
     }
 
+    public void addNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < this.storedData.Count; i++)
+        {
+            Console.WriteLine(storedData[i]);
+        }
+    }
+}
+
+class program
+{
     static void Main(string[] args)
     {
-        Console.WriteLine(JumlahTigaAngka<float>(13, 2, 22));
+        SimpleDataBase<float> data = new SimpleDataBase<float>();
+        data.addNewData(13);
+        data.addNewData(02);
+        data.addNewData(22);
+
+        data.PrintAllData();
     }
 }
