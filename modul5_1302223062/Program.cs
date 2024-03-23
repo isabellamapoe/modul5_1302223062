198e39718b8df9f25a25d19fb7dcbac5077cbc8b
rifkyfrds <rifky.frds@gmail.com>
2024-03-23 07:44:36 +0700

    Add project files.

---
+++ modul5_1302223062/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul5_1302223062
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
